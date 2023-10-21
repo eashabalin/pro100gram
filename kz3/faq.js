81510d7ec4589184d8ba97ad5ed533a79f0f6e8d
Alfa <alfa@MacBook-Pro-Alfa.local>
2023-10-21 12:46:33 +0300

    kz3

--- kz3/faq.js
+++ kz3/faq.js
@@ -1,6 +1,6 @@
 function toggleText(id) {
     var x = document.getElementById("q" + String(id));
-    if (x.style.display === "none") {
+    if (x.style.display === "none" || x.style.display === "") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
